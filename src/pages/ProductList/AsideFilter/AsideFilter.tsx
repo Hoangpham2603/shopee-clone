f17ef229becb0e928c6a7de43d57f22df9db7bc4
hoangpham2603 <kunpham2603@gmail.com>
2023-07-11 09:04:24 -0700

    working desktop

--- src/pages/ProductList/AsideFilter/AsideFilter.tsx
+++ src/pages/ProductList/AsideFilter/AsideFilter.tsx
@@ -9,6 +9,7 @@ import { useForm, Controller } from 'react-hook-form'
 import { Schema, schema } from '../../../utils/rules'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { NoUndefinedFiled } from '../../../types/utils.type'
+import RatingStars from '../RatingStarts'
 interface Props {
   queryConfig: QueryConfig
   categories: Category[] | []
@@ -75,41 +76,6 @@ export default function AsideFilter({ queryConfig, categories }: Props) {
       </Link>
 
       <div className='my-4 h-[1px] bg-gray-300'></div>
-
-      <ul>
-        {categories.map((categoryItem) => {
-          const isActive = category === categoryItem._id
-          return (
-            <li key={categoryItem._id} className='py-2 pl-2'>
-              <Link
-                to={{
-                  pathname: path.home,
-                  search: createSearchParams({
-                    ...queryConfig,
-                    category: categoryItem._id
-                  }).toString()
-                }}
-                className={classNames('relative px-2', {
-                  'font-semibold text-orange': isActive
-                })}
-              >
-                {isActive && (
-                  <svg viewBox='0 0 4 7' className='absolute left-[-10px] top-1 h-2 w-2 fill-orange'>
-                    <polygon points='4 3.5 0 0 0 7' />
-                  </svg>
-                )}
-                {categoryItem.name}
-              </Link>
-            </li>
-          )
-        })}
-
-        <li className='py-2pl-2'>
-          <Link to={path.home} className='relative px-2 font-semibold text-orange'>
-            Phone
-          </Link>
-        </li>
-      </ul>
 
       <Link to={path.home} className='mt-4 flex items-center font-bold uppercase'>
         <svg
@@ -190,74 +156,10 @@ export default function AsideFilter({ queryConfig, categories }: Props) {
       </div>
       <div className='my-4 h-[1px] bg-gray-300'></div>
       <div className='text-sm'>Rating</div>
-      <ul className='my-3'>
-        <li className='py-1 pl-2'>
-          {/* STARS */}
-          <Link to='' className='flex items-center text-sm'>
-            {Array(5)
-              .fill(0)
-              .map((_, index) => (
-                <svg viewBox='0 0 9.5 8' className='mr-1 h-4 w-4' key={index}>
-                  <defs>
-                    <linearGradient id='ratingStarGradient' x1='50%' x2='50%' y1='0%' y2='100%'>
-                      <stop offset={0} stopColor='#ffca11' />
-                      <stop offset={1} stopColor='#ffad27' />
-                    </linearGradient>
-                    <polygon
-                      id='ratingStar'
-                      points='14.910357 6.35294118 12.4209136 7.66171903 12.896355 4.88968305 10.8823529 2.92651626 13.6656353 2.52208166 14.910357 0 16.1550787 2.52208166 18.9383611 2.92651626 16.924359 4.88968305 17.3998004 7.66171903'
-                    />
-                  </defs>
-                  <g fill='url(#ratingStarGradient)' fillRule='evenodd' stroke='none' strokeWidth={1}>
-                    <g transform='translate(-876 -1270)'>
-                      <g transform='translate(155 992)'>
-                        <g transform='translate(600 29)'>
-                          <g transform='translate(10 239)'>
-                            <g transform='translate(101 10)'>
-                              <use stroke='#ffa727' strokeWidth='.5' xlinkHref='#ratingStar' />
-                            </g>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-              ))}
-          </Link>
 
-          <Link to='' className='flex items-center text-sm'>
-            {Array(5)
-              .fill(0)
-              .map((_, index) => (
-                <svg viewBox='0 0 9.5 8' className='mr-1 h-4 w-4' key={index}>
-                  <defs>
-                    <linearGradient id='ratingStarGradient' x1='50%' x2='50%' y1='0%' y2='100%'>
-                      <stop offset={0} stopColor='#ffca11' />
-                      <stop offset={1} stopColor='#ffad27' />
-                    </linearGradient>
-                    <polygon
-                      id='ratingStar'
-                      points='14.910357 6.35294118 12.4209136 7.66171903 12.896355 4.88968305 10.8823529 2.92651626 13.6656353 2.52208166 14.910357 0 16.1550787 2.52208166 18.9383611 2.92651626 16.924359 4.88968305 17.3998004 7.66171903'
-                    />
-                  </defs>
-                  <g fill='url(#ratingStarGradient)' fillRule='evenodd' stroke='none' strokeWidth={1}>
-                    <g transform='translate(-876 -1270)'>
-                      <g transform='translate(155 992)'>
-                        <g transform='translate(600 29)'>
-                          <g transform='translate(10 239)'>
-                            <g transform='translate(101 10)'>
-                              <use stroke='#ffa727' strokeWidth='.5' xlinkHref='#ratingStar' />
-                            </g>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-              ))}
-          </Link>
-        </li>
-      </ul>
+      {/* STARS */}
+      <RatingStars />
+
       <div className='my-4 h-[1px] bg-gray-300'></div>
 
       <Button className='hover:bg-orange-80 flex w-full items-center justify-center bg-orange p-2 text-sm uppercase text-white'>
--- src/pages/ProductList/ProductDetails/ProductDetails.tsx
+++ src/pages/ProductList/ProductDetails/ProductDetails.tsx
@@ -16,7 +16,7 @@ export default function ProductDetails() {
       <div className='bg-white py-4 shadow'>
         <div className='container'>
           <div className='grid grid-cols-12 gap-9'>
-            <div className='cols-span-5'>
+            <div className='col-span-5'>
               <div className='relative w-full pt-[100%] shadow'>
                 <img
                   src={product?.image}
@@ -38,7 +38,39 @@ export default function ProductDetails() {
                     <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
                   </svg>
                 </button>
+
+                {product?.images.slice(0, 5).map((img, index) => {
+                  const isActive = index === 0
+                  return (
+                    <div className='relative w-full pt-[100%]' key={img}>
+                      <img
+                        src={img}
+                        alt={product?.name}
+                        className='absolute left-0 top-0 h-full w-full cursor-pointer bg-white object-cover'
+                      />
+
+                      {isActive && <div className='absolute inset-0 border-2 border-orange'></div>}
+                    </div>
+                  )
+                })}
+
+                <button className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='h-5 w-5'
+                  >
+                    <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
+                  </svg>
+                </button>
               </div>
+            </div>
+
+            <div className='col-span-7'>
+              <h1 className='text-xl font-medium uppercase'>{product?.name}</h1>
             </div>
           </div>
         </div>
