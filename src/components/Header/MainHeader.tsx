63eb755e45d8cee13a560cd0277af76546082495
Hoangpham2603 <kunpham2603@gmail.com>
2023-07-22 16:00:18 -0700

    testing

--- src/components/Header/MainHeader.tsx
+++ src/components/Header/MainHeader.tsx
@@ -138,17 +138,11 @@ export default function MainHeader() {
                     d='M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z'
                   />
                 </svg>
-<<<<<<< HEAD
-                <span className='absolute right-[-9spx] top-[-5px] rounded-full bg-white px-[9px] py-[1px] text-xs text-orange'>
-                  {purchasesInCart?.length}
-                </span>
-=======
                 {purchasesInCart && (
                   <span className='absolute right-[-10px] top-[-5px] rounded-full bg-white px-[9px] py-[1px] text-xs text-orange'>
                     {purchasesInCart?.length}
                   </span>
                 )}
->>>>>>> c56db6fe2f395f99c62a02f2754f652d99f14057
               </Link>
             </Popover>
           </div>
